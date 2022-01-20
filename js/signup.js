aab772065afb57af72c059e621900a1366f76f30
KLMS <kaspars.klims@gmail.com>
2022-01-21 00:16:46 +0200

    some changes

--- js/signup.js
+++ js/signup.js
@@ -1,3 +1,4 @@
+const form = document.querySelector(`#form`);
 const userName = document.querySelector(`#name`);
 const userLastName = document.querySelector(`#lastname`);
 const userEmail = document.querySelector(`#email`);
@@ -6,7 +7,9 @@ const pass2 = document.querySelector(`#pass2`);
 const checkbox = document.querySelector(`#checkbox`);
 const userSubmit = document.querySelector(`#submit`);
 const regxEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const showError = function (message) {
+const showError = function (message, field) {
+    const smallEl = document.createElement(`small`);
+    field.insertAdjacentElement(`beforebegin`, smallEl);
     const small = document.querySelector(`small`);
     small.classList.add(`error`);
     small.innerHTML = message;
@@ -18,33 +21,54 @@ const allOk = function () {
 };
 const checkLenght = function (field, min, max) {
     if (field.value.length > max) {
-        showError(`${field} should be less than ${max} symbols`);
+        showError(`Should be less than ${max} symbols`, field);
     }
     else if (field.value.length < min) {
-        showError(`${field} should be more than ${min} symbols`);
+        showError(`Should be more than ${min} symbols`, field);
     }
     else {
         allOk();
     }
 };
 const checkEmail = function (field) {
-    if (field.value === "") {
-        return showError(`Email adress is required`);
+    if (!regxEmail.test(field.value)) {
+        showError(`Please provide a valid e-mail adress`, field);
     }
-    else if (!regxEmail.test(field.value.trim())) {
-        return showError(`Please provide a valid e-mail adress`);
+    else {
+        allOk();
     }
-};
-const loopElements = function (arr) {
-    arr.forEach((curr) => checkLenght(curr, 3, 12));
 };
 const checkPassword = function (field1, field2) {
     if (field1.value != field2.value) {
-        field2.insertAdjacentHTML(`afterend`, `<small>${showError(`Password don't match!`)} </small>`);
+        const small = document.createElement(`small`);
+        field2.insertAdjacentElement(`beforebegin`, small);
+        showError(`Password's don't match!`, field2);
     }
 };
-userSubmit.addEventListener(`submit`, (e) => {
+const requireField = function (arr) {
+    arr.forEach((curr) => {
+        curr.placeholder = "* Required";
+        showError(``, curr);
+    });
+};
+const checkForm = function () {
+    if (checkbox.checked) {
+        showError(``, checkbox);
+        checkLenght(userLastName, 2, 18);
+        requireField([userName, userEmail, userLastName, pass1, pass2]);
+        checkPassword(pass1, pass2);
+        checkEmail(userEmail);
+        checkLenght(userName, 2, 12);
+    }
+    else if (!checkbox.checked) {
+        showError(`You must accept terms and conditions`, checkbox);
+    }
+    else {
+        form.submit();
+    }
+};
+userSubmit.addEventListener(`click`, (e) => {
     e.preventDefault();
-    checkPassword(pass1, pass2);
+    checkForm();
 });
 //# sourceMappingURL=signup.js.map
--- js/signup.ts
+++ js/signup.ts
@@ -1,3 +1,4 @@
+const form = document.querySelector(`#form`)! as HTMLFormElement;
 const userName = document.querySelector(`#name`)! as HTMLInputElement;
 const userLastName = document.querySelector(`#lastname`)! as HTMLInputElement;
 const userEmail = document.querySelector(`#email`)! as HTMLInputElement;
@@ -8,7 +9,9 @@ const userSubmit = document.querySelector(`#submit`)! as HTMLInputElement;
 const regxEmail =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-const showError = function (message: string) {
+const showError = function (message: string, field: HTMLInputElement) {
+  const smallEl = document.createElement(`small`);
+  field.insertAdjacentElement(`beforebegin`, smallEl);
   const small = document.querySelector(`small`);
   small.classList.add(`error`);
   small.innerHTML = message;
@@ -25,24 +28,20 @@ const checkLenght = function (
   max: number
 ) {
   if (field.value.length > max) {
-    showError(`${field} should be less than ${max} symbols`);
+    showError(`Should be less than ${max} symbols`, field);
   } else if (field.value.length < min) {
-    showError(`${field} should be more than ${min} symbols`);
+    showError(`Should be more than ${min} symbols`, field);
   } else {
     allOk();
   }
 };
 
 const checkEmail = function (field: HTMLInputElement) {
-  if (field.value === "") {
-    return showError(`Email adress is required`);
-  } else if (!regxEmail.test(field.value.trim())) {
-    return showError(`Please provide a valid e-mail adress`);
+  if (!regxEmail.test(field.value)) {
+    showError(`Please provide a valid e-mail adress`, field);
+  } else {
+    allOk();
   }
-};
-
-const loopElements = function (arr: Array<HTMLInputElement>) {
-  arr.forEach((curr) => checkLenght(curr, 3, 12));
 };
 
 const checkPassword = function (
@@ -50,14 +49,34 @@ const checkPassword = function (
   field2: HTMLInputElement
 ) {
   if (field1.value != field2.value) {
-    field2.insertAdjacentHTML(
-      `afterend`,
-      `<small>${showError(`Password don't match!`)} </small>`
-    );
+    const small = document.createElement(`small`);
+    field2.insertAdjacentElement(`beforebegin`, small);
+    showError(`Password's don't match!`, field2);
+  }
+};
+const requireField = function (arr: Array<HTMLInputElement>) {
+  arr.forEach((curr) => {
+    curr.placeholder = "* Required";
+    showError(``, curr);
+  });
+};
+
+const checkForm = function () {
+  if (checkbox.checked) {
+    showError(``, checkbox);
+    checkLenght(userLastName, 2, 18);
+    requireField([userName, userEmail, userLastName, pass1, pass2]);
+    checkPassword(pass1, pass2);
+    checkEmail(userEmail);
+    checkLenght(userName, 2, 12);
+  } else if (!checkbox.checked) {
+    showError(`You must accept terms and conditions`, checkbox);
+  } else {
+    form.submit();
   }
 };
 
 userSubmit.addEventListener(`click`, (e) => {
   e.preventDefault();
-  checkPassword(pass1, pass2);
+  checkForm();
 });
